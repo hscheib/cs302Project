3bda69a2f991b35369e1847373afca742637b12d
Hunter Scheib <hssth5@mst.edu>
2012-12-05 22:42:14 -0600

    Finished PersonTest.java tests

--- src/test/PersonTest.java
+++ src/test/PersonTest.java
@@ -58,5 +58,76 @@ public class PersonTest {
 		assertEquals(p.getPhoneNumber(), "1234567890");
 	}
 
+	@Test
+	public void changePersonAddress() {
+		Person p = getTestPerson();
+
+		assertEquals(p.getAddress(), "123 Danger Dr");
+
+		p.setAddress("321 Easy St");
+
+		assertEquals(p.getAddress(), "321 Easy St");
+
+	}
+	
+	@Test
+	public void changePersonCity() {
+		Person p = getTestPerson();
+
+		assertEquals(p.getCity(), "Rolla");
+
+		p.setCity("Chicago");
+
+		assertEquals(p.getCity(), "Chicago");
+
+	}
+	
+	@Test
+	public void changePersonState() {
+		Person p = getTestPerson();
+
+		assertEquals(p.getState(), "MO");
+
+		p.setState("IL");
+
+		assertEquals(p.getState(), "IL");
+
+	}
+	
+	@Test
+	public void changePersonZip() {
+		Person p = getTestPerson();
+
+		assertEquals(p.getZip(), "65401");
+
+		p.setZip("33333");
+
+		assertEquals(p.getZip(), "33333");
+
+	}
+	
+	@Test
+	public void changePersonPhoneNumber() {
+		Person p = getTestPerson();
+
+		assertEquals(p.getPhoneNumber(), "1234567890");
+
+		p.setPhoneNumber("5556664444");
+
+		assertEquals(p.getPhoneNumber(), "5556664444");
+
+	}
+
+	private Person getTestPerson() {
+		Person p = null;
+		
+		try {
+			p = new Person("Bob", "Smith", "123 Danger Dr", "Rolla", "MO", "65401", "1234567890");
+		} catch (Exception e) {
+			fail("Should not throw exception");
+		}
+
+		return p;
+	}
 
 }
