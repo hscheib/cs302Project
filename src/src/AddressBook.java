5cd25ad0e68691836ce848384b5d06494a1a7bc5
Hunter Scheib <hssth5@mst.edu>
2012-12-05 22:46:18 -0600

    Finished tests and AddressBook.java

--- src/src/AddressBook.java
+++ src/src/AddressBook.java
@@ -1,13 +1,48 @@
 package src;
 
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.LinkedList;
+import java.util.List;
+
 public class AddressBook {
+
+	private Hashtable<String, Person> people = new Hashtable<String, Person>();
 
 	public AddressBook() {
 
 	}
 
 	public int size() {
-		return 0;
+		return people.size();
 	}
-	
+
+	public void addPerson(Person person) throws Exception {
+		if (!people.containsKey(person.getFirstName() + person.getLastName()))
+			people.put(person.getFirstName() + person.getLastName(), person);
+		else
+			throw new Exception("Duplicate Name");
+
+	}
+
+	public List<Person> getAllPeople() {
+		List<Person> peopleList = new LinkedList<Person>();
+
+		Enumeration<Person> enumerationOfPeople = people.elements();
+
+		while (enumerationOfPeople.hasMoreElements()) {
+			peopleList.add(enumerationOfPeople.nextElement());
+		}
+		
+		return peopleList;
+	}
+
+	public Person findPersonByName(Person person) {
+		return people.get(person.getFirstName() + person.getLastName());
+	}
+
+	public void updatePerson(Person person) {
+		people.put(person.getFirstName() + person.getLastName(), person);
+	}
+
 }
--- src/test/AddressBookTest.java
+++ src/test/AddressBookTest.java
@@ -2,6 +2,9 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.junit.Test;
 
 import src.AddressBook;
@@ -15,5 +18,127 @@ public class AddressBookTest {
 		assertEquals(ab.size(), 0);
 	}
 
+	@Test
+	public void addOnePersonToAddressBook() {
+		AddressBook ab = new AddressBook();
+
+		Person p = getTestPerson();
+
+		try {
+			ab.addPerson(p);
+		} catch (Exception e) {
+			fail("Should not throw exception");
+		}
+		assertEquals(ab.size(), 1);
+
+	}
+
+	@Test
+	public void checkNewEntryInAddressBook() {
+		AddressBook ab = new AddressBook();
+
+		Person p = getTestPerson();
+
+		assertEquals("Bob", p.getFirstName());
+		assertEquals("Smith", p.getLastName());
+
+		try {
+			ab.addPerson(p);
+		} catch (Exception e) {
+			fail("Should not throw exception");
+		}
+
+		List<Person> people = ab.getAllPeople();
+
+		assertEquals("Bob", people.get(0).getFirstName());
+		assertEquals("Smith", people.get(0).getLastName());
+
+	}
+
+	@Test
+	public void tryToAddDuplicateName() {
+		AddressBook ab = new AddressBook();
+
+		Person p1 = null;
+		Person p2 = null;
+
+		try {
+			p1 = new Person("Bob", "Smith");
+			p2 = new Person("Bob", "Smith");
+		} catch (Exception e) {
+			fail("Should not throw exception");
+		}
+
+		try {
+			ab.addPerson(p1);
+		} catch (Exception e) {
+			fail("Should not throw exception");
+		}
+
+		try {
+			ab.addPerson(p2);
+			fail("Expected to fail");
+		} catch (Exception e) {
+			// PASSING SCENARIO
+		}
+
+		assertEquals(ab.size(), 1);
+
+	}
+
+	@Test
+	public void lookUpPersonByName() {
+		AddressBook ab = new AddressBook();
+
+		Person p = getTestPerson();
+
+		try {
+			ab.addPerson(p);
+		} catch (Exception e) {
+			fail("Should not throw exception");
+		}
+
+		Person p2 = ab.findPersonByName(p);
+
+		assertEquals("Bob", p2.getFirstName());
+		assertEquals("Smith", p2.getLastName());
+
+	}
+	
+	@Test
+	public void updatePersonAddress() {
+		AddressBook ab = new AddressBook();
+
+		Person p = getTestPerson();
+		
+		assertEquals("123 Danger Dr", p.getAddress());
+		
+		try {
+			ab.addPerson(p);
+		} catch (Exception e) {
+			fail("Should not throw exception");
+		}
+		
+		Person p2 = ab.findPersonByName(p);
+		p2.setAddress("321 Easy St");
+		
+		ab.updatePerson(p2);
+		
+		Person p3 = ab.findPersonByName(p);
+		assertEquals("321 Easy St", p3.getAddress());
+		
+	}
+	
+
+	private Person getTestPerson() {
+		Person p = null;
+		try {
+			p = new Person("Bob", "Smith", "123 Danger Dr", "Rolla", "MO", "65401", "1234567890");
+		} catch (Exception e) {
+			fail("Should not throw exception");
+		}
+
+		return p;
+	}
 
 }
