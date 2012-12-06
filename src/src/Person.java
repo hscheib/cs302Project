3c7052040a4ba60cdd5ab859309e2b7ea961024a
Hunter Scheib <hssth5@mst.edu>
2012-12-05 22:41:37 -0600

    updated Person.java and PersonTest.java

--- src/src/Person.java
+++ src/src/Person.java
@@ -2,10 +2,81 @@ package src;
 
 public class Person {
 
-	public Person() {
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String state;
+	private String zip;
+	private String phoneNumber;
 
+	public Person(String first, String last) throws Exception {
+		if(first == null || last ==null){
+			throw new Exception();
+		}
+		this.firstName = first;
+		this.lastName = last;
 	}
 
+	public Person(String first, String last, String addr, String city, String state, String zip, String phoneNo) throws Exception {
+		if(first == null || last ==null){
+			throw new Exception();
+		}
+		this.firstName = first;
+		this.lastName = last;
+		this.address = addr;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNumber = phoneNo;
+	}
 
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
 
 }
--- src/test/PersonTest.java
+++ src/test/PersonTest.java
@@ -7,9 +7,56 @@ import org.junit.Test;
 public class PersonTest {
 
 	@Test
-	public void createPerson() {
-		Person p = new Person(null, "Smith");
+	public void createPersonWithInvalidFirstName() {
+		try {
+			Person p = new Person(null, "Smith");
+			fail("Expected to fail");
+		} catch (Exception e) {
+			// PASSING SCENARIO
+		}
 
 	}
+
+	@Test
+	public void createPersonWithInvalidLastName() {
+		try {
+			Person p = new Person("Bob", null);
+			fail("Expected to fail");
+		} catch (Exception e) {
+			// PASS SCENARIO
+
+		}
+	}
+
+	@Test
+	public void createPersonWithMinimumRequirements() {
+		Person p = null;
+		try {
+			p = new Person("Bob", "Smith");
+		} catch (Exception e) {
+			fail("Should not throw exception");
+		}
+		assertEquals(p.getFirstName(), "Bob");
+		assertEquals(p.getLastName(), "Smith");
+	}
+
+	@Test
+	public void createPersonWithFullRequirements() {
+		Person p = null;
+		try {
+			p = new Person("Bob", "Smith", "123 Danger Dr", "Rolla", "MO", "65401", "1234567890");
+		} catch (Exception e) {
+			fail("Should not throw exception");
+		}
+
+		assertEquals(p.getFirstName(), "Bob");
+		assertEquals(p.getLastName(), "Smith");
+		assertEquals(p.getAddress(), "123 Danger Dr");
+		assertEquals(p.getCity(), "Rolla");
+		assertEquals(p.getState(), "MO");
+		assertEquals(p.getZip(), "65401");
+		assertEquals(p.getPhoneNumber(), "1234567890");
+	}
+
 
 }
